f20b0a1b25fb05709216ea2141227823c0f25d51
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-01-15 19:40:51 +0630

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,11 +5,11 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://www.thelapyae.com'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
-      aria-label='View source on GitHub'
+      aria-label='visit my page'
     >
       
     </a>
