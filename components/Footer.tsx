ddbd9b53dedfed994242760ff52c60fbdcc58c27
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-01-16 14:06:58 +0630

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2022 {config.author} All Right Reserve </div>
 
       {hasMounted ? (
         <div className={styles.settings}>
