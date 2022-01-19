23e151670acb9bee4451180667ca714e59a17b98
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-01-19 17:04:33 +0630

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -23,6 +23,13 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
 
       <meta name='theme-color' content='#F3C12B' />
       <meta property='og:type' content='website' />
+      <meta property='og:image' content='https://drive.google.com/uc?id=1sr_ZFLGNq-f49hJV_8Ifql_7tgFwS5If' />
+      
+      <meta property='twitter:card' content='https://drive.google.com/uc?id=1sr_ZFLGNq-f49hJV_8Ifql_7tgFwS5If' />
+      <meta property='twitter:url' content='https://www.thelapyae.com/' />
+      <meta property='twitter:title' content='TheLaPyae' />
+      <meta property='twitter:description' content='TheLaPyae - No Code Brand Strategist From Yangon' />
+      <meta property='twitter:image' content='https://drive.google.com/uc?id=1sr_ZFLGNq-f49hJV_8Ifql_7tgFwS5If'/>
     </Head>
   )
 }
