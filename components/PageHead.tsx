c21de894f9b550a5a7ca6f689b053f4fe9791721
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-04-18 09:51:45 +0630

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -38,12 +38,12 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
       // from here
       
      <Script src="https://tally.so/widgets/embed.js" strategy="lazyOnload"
-       id="embed"
-       nonce="mY9bdm"
-       data-test="embed"
-       />
-       
-
+       {Tally.openPopup('mY9bdm', {
+  layout: 'modal', // Open as a centered modal
+  width: 700, // Set the width of the modal
+  autoClose: 5000, // Close the popup 5 seconds after form was submitted (in ms)
+});}
+/>
 
       
       // I did edit to here 
