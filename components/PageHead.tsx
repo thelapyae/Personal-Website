ac417aec5aef9df1c0b2eae5e075b42311409f6d
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-04-18 09:29:42 +0630

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -37,7 +37,24 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
       
       // from here
       
-     <Script src="https://tally.so/widgets/embed.js" />
+     <Script src="https://tally.so/widgets/embed.js">
+       {      
+       window.TallyConfig = {
+  "formId": "mY9bdm",
+  "popup": {
+    "emoji": {
+      "text": "👋",
+      "animation": "wave"
+    },
+    "open": {
+      "trigger": "time",
+      "ms": 20000
+    },
+    "overlay": true
+  }
+}; 
+        } }
+       </Script>
 
       
       // I did edit to here 
