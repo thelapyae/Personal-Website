520ed9d14ecd7cc9e85e8f1bbd906633141d0473
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-05-04 14:45:35 +0630

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Popup from 'reactjs-popup';
 import { domain } from 'lib/config'
 import { resolveNotionPage } from 'lib/resolve-notion-page'
 import { NotionPage } from 'components'
 
 
+export default function PopupGfg(){
+  return(
+  Tally.openPopup('mY9bdm', {
+  layout: 'modal', // Open as a centered modal
+  width: 700, // Set the width of the modal
+  autoClose: 5000, // Close the popup 5 seconds after form was submitted (in ms)
+});
+  )
+};
 
+// edited to here
 
 export const getStaticProps = async () => {
   try {
